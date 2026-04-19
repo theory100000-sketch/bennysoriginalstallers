6746172da127f428615d5b51d08c22c6dcdc4f35
theory100000-sketch <theory100000@gmail.com>
2026-04-19 14:12:22 +0200

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import BennysOriginalDashboard from './App.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BennysOriginalDashboard />
-  </React.StrictMode>
-);
